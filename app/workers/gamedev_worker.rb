c54b3b9a69c0251b8a76ac5f97698f6e702b2627
KarenVentura <powercodegirls@gmail.com>
2015-08-06 10:27:01 -0500

    Improve code according HoundCI comments

--- app/workers/gamedev_worker.rb
+++ app/workers/gamedev_worker.rb
@@ -2,13 +2,13 @@ class GamedevWorker
   include Sidekiq::Worker
 
   def perform
-    response = RubyStackoverflow.questions({
-      order: 'asc',
-      filter: 'withbody',
-      pagesize: 100 })
+    response = RubyStackoverflow.questions(
+      order: "asc",
+      filter: "withbody",
+      pagesize: 100)
     response.data.each do |question|
       p = ExternalPost.new(
-      	title: question.title,
+        title: question.title,
         user_display_name: question.owner[:display_name],
         body_html: question.body,
         source_url: question.link,
@@ -18,7 +18,7 @@ class GamedevWorker
   end
 
   Sidekiq::Cron::Job.create(
-  	name: "RubyStackoverflow fetch questions every friday",
+    name: "RubyStackoverflow fetch questions every friday",
     cron: "* * * * 5", 
     klass: "GamedevWorker")
 end
